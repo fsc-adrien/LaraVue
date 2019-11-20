f7a157db5f1d971703584b6c2a58dc7974decb2f
ThienBD-BU1-FE <thienbd@ominext.com>
2019-11-20 09:34:01 +0700

    The first commit of FEman

---
+++ resources/assets/admin/js/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="wrapper" style="position: relative">
+        <app-header></app-header>
+        <app-aside></app-aside>
+        <div class="content-wrapper">
+<!--            <app-breadcrumb :list="list"></app-breadcrumb>-->
+<!--            <section class="content">-->
+<!--                <router-view></router-view>-->
+<!--            </section>-->
+        </div>
+        <app-footer></app-footer>
+        <app-control-sidebar></app-control-sidebar>
+        <div class="control-sidebar-bg"></div>
+    </div>
+</template>
+
+<script>
+    import AppHeader from "./components/header/Header";
+    import AppAside from "./components/aside/Aside";
+    import AppBreadcrumb from "./components/breadcrumb/Breadcrumb";
+    import AppFooter from "./components/footer/Footer";
+    import AppControlSidebar from "./components/control_siderbar/ControlSidebar";
+
+    export default {
+        name: "App",
+        components: {
+            AppHeader,
+            AppAside,
+            AppBreadcrumb,
+            AppControlSidebar,
+            AppFooter,
+        }
+    }
+</script>
