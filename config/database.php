33ed4401e553601090830f6d0304339c8f181850
ThienBD-BU1-FE <thienbd@ominext.com>
2019-11-21 16:21:44 +0700

    dep trai lam gi cung dung =)) 2222

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$DATABASE_URL=parse_url('postgres://ottxssqvghqbaa:ea1a58d97700a619747d438e8380f013312002a228e6a3c437c0c7c449f9e8a7@ec2-107-21-113-60.compute-1.amazonaws.com:5432/d108of1maopqff');
 return [
 
     /*
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
